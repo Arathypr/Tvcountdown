695cb21da9d07b6b4b881c641b09b48cfb71a310
Arathy <arathyp.radhakrishnan@gmail.com>
2023-10-30 11:04:38 +0530

    tvcompeted

--- src/components/countdown/CountDown.jsx
+++ src/components/countdown/CountDown.jsx
@@ -1,108 +1,124 @@
-import React, { useEffect, useState } from "react";
-import { getRemainingTimeUntilMsTimestamp } from "./utils/CountDownUtils";
-
+import React, { useState, useEffect, useRef } from "react";
 import Seconds from "../seconds/Seconds";
-import "./CountDown.css";
-const defaultRemainingTime = {
-  SEC: "00",
-  MIN: "00",
-  HOURS: "00",
-  DAYS: "00",
-};
 
 function CountDown() {
-  const [countdownZero, setCountdownZero] = useState(false); // Initialize countdownZero state
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMin, setTimerMin] = useState("00");
+  const [timerSec, setTimerSec] = useState("00");
+  const [countdownZero, setCountdownZero] = useState(false);
+  let interval = useRef();
 
-  // Calculate the timestamp for October 20, 2023, 11:35 PM IST
-  const startDate = new Date("2023-10-21T06:19:00");
-  const countdownStartTimestampMs = startDate.getTime();
+  const startTimer = () => {
+    const countDownDate = new Date("January 07, 2024 10:00:00").getTime();
 
-  // Calculate the timestamp for January 7, 2024, 10:00 AM IST
-  // const endDate = new Date("2024-01-07T10:00:00");
-  const endDate = new Date("2024-01-07T10:00:00");
-  const countdownEndTimestampMs = endDate.getTime();
+    interval.current = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
 
-  const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+      if (distance <= 0) {
+        clearInterval(interval.current);
+        console.log("Countdown reached zero.");
+        setCountdownZero(true);
+      } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const sec = Math.floor((distance % (1000 * 60)) / 1000);
+
+        console.log("Days:", days);
+        console.log("Hours:", hours);
+        console.log("Minutes:", min);
+        console.log("Seconds:", sec);
+
+        setTimerDays(String(days).padStart(2, "0"));
+        setTimerHours(String(hours).padStart(2, "0"));
+        setTimerMin(String(min).padStart(2, "0"));
+        setTimerSec(String(sec).padStart(2, "0"));
+
+        if (days === 0 && hours === 0 && min === 0 && sec === 0) {
+          clearInterval(interval.current);
+          setCountdownZero(true);
+        }
+      }
+    }, 1000);
+  };
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      updateRemainingTime(countdownEndTimestampMs);
-    }, 1000);
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
 
-    return () => clearInterval(intervalId);
-  }, [countdownEndTimestampMs]);
-
-  function updateRemainingTime(endTime) {
-    const updatedRemainingTime = getRemainingTimeUntilMsTimestamp(endTime);
-    setRemainingTime(updatedRemainingTime);
-
-    if (
-      updatedRemainingTime.DAYS === "00" &&
-      updatedRemainingTime.HOURS === "00" &&
-      updatedRemainingTime.MIN === "00"
-    ) {
-      setCountdownZero(true);
-    } else {
-      setCountdownZero(false);
-    }
-  }
   return (
     <>
-      <div className="font-Montserrat text-center sm:space-x-16zz">
-        <div className="md:hidden vvsm:mt-1  mt-40 sm:mt-36 sm:mr-20">
-          <div className="flex flex-col items-center space-x-1 ">
-            <span className="text-5xl font-extrabold sm:text-7xl vvsm:text-5xl ">
-              {remainingTime.DAYS}
-            </span>
-            <span className="text-sm sm:text-base sm:pb-10 vsm:pb-14 vvsm:pb-3">
-              DAYS
-            </span>
-          </div>
-          <div className="flex flex-col items-center space-x-1">
-            <span className="text-6xl font-extrabold sm:text-7xl vvsm:text-5xl">
-              {remainingTime.HOURS}
-            </span>
-            <span className="text-sm sm:text-base sm:pb-10 vsm:pb-14 vvsm:pb-3">
-              HOURS
-            </span>
-          </div>
-          <div className="flex flex-col items-center space-x-1">
-            <span className="text-6xl font-extrabold sm:text-7xl vvsm:text-5xl">
-              {remainingTime.MIN}
-            </span>
-            <span className="text-sm sm:text-base">MIN</span>
-          </div>
-        </div>
+      <div className="font-Montserrat text-center sm:space-x-16">
+        {countdownZero ? (
+          <p className="font-bold text-3xl text-center">The wait is over</p>
+        ) : (
+          <>
+            <div className="md:hidden vvsm:mt-1  mt-40 sm:mt-36 sm:mr-20">
+              <div className="flex flex-col items-center space-x-1 ">
+                <span className="text-5xl font-extrabold sm:text-7xl vvsm:text-5xl ">
+                  {timerDays}
+                </span>
+                <span className="text-sm sm:text-base sm:pb-10 vsm:pb-14 vvsm:pb-3">
+                  DAYS
+                </span>
+              </div>
+              <div className="flex flex-col items-center space-x-1">
+                <span className="text-6xl font-extrabold sm:text-7xl vvsm:text-5xl">
+                  {timerHours}
+                </span>
+                <span className="text-sm sm:text-base sm:pb-10 vsm:pb-14 vvsm:pb-3">
+                  HOURS
+                </span>
+              </div>
+              <div className="flex flex-col items-center space-x-1">
+                <span className="text-6xl font-extrabold sm:text-7xl vvsm:text-5xl">
+                  {timerMin}
+                </span>
+                <span className="text-sm sm:text-base">MIN</span>
+              </div>
+            </div>
 
-        <div className="hidden md:flex flex-row font-Montserrat text-center space-x-3">
-          <div className="flex items-center space-x-1">
-            <span className="text-6xl font-extrabold sm:text-5xl">
-              {remainingTime.DAYS}
-            </span>
-            <span className="text-sm sm:text-base">DAYS</span>
-          </div>
-          <div className="flex items-center space-x-1">
-            <span className="text-6xl font-extrabold sm:text-5xl">
-              {remainingTime.HOURS}
-            </span>
-            <span className="text-sm sm:text-base">HOURS</span>
-          </div>
-          <div className="flex items-center space-x-1">
-            <span className="text-6xl font-extrabold sm:text-5xl">
-              {remainingTime.MIN}
-            </span>
-            <span className="text-sm sm:text-base">MIN</span>
-          </div>
-        </div>
-      </div>
+            <div className="hidden md:flex flex-row font-Montserrat text-center space-x-3">
+              <div className="flex items-center space-x-1">
+                <span className="text-6xl font-extrabold sm:text-5xl">
+                  {timerDays}
+                </span>
+                <span className="text-sm sm:text-base">DAYS</span>
+              </div>
+              <div className="flex items-center space-x-1">
+                <span className="text-6xl font-extrabold sm:text-5xl">
+                  {timerHours}
+                </span>
+                <span className="text-sm sm:text-base">HOURS</span>
+              </div>
+              <div className="flex items-center space-x-1">
+                <span className="text-6xl font-extrabold sm:text-5xl">
+                  {timerMin}
+                </span>
+                <span className="text-sm sm:text-base">MIN</span>
+              </div>
+            </div>
 
-      <div className="flex items-center ml-[68px] ">
-        <span className="font-extrabold">
-          <Seconds countdownZero={countdownZero} />
-        </span>
-        <span className="text-base sec" style={{ marginLeft: "-120px" }}>
-          SEC
-        </span>
+            <div className="flex items-center " style={{ marginLeft: "115%" }}>
+              <span className="font-extrabold">
+                <Seconds countdownZero={countdownZero} />
+              </span>
+              <span
+                className="text-base sec"
+                style={{ marginLeft: "-115px", marginTop: "-105px" }}
+              >
+                SEC
+              </span>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
--- src/components/seconds/Seconds.jsx
+++ src/components/seconds/Seconds.jsx
@@ -2,16 +2,40 @@ import React, { useState, useEffect } from "react";
 import secondwhite from "../../assets/secondwhite.svg";
 import transnightmode from "../../assets/secondtransnightmode_.svg";
 import "./Seconds.css";
+import CountDown from "../countdown/CountDown";
 
 function Seconds({ countdownZero }) {
   const [rotationDegree, setRotationDegree] = useState(0);
+  const [shouldRunAnimation, setShouldRunAnimation] = useState(true);
+  console.log("connt", countdownZero);
 
   useEffect(() => {
+    const now = new Date();
+    const initialSeconds = now.getSeconds();
+    const degreesPerSecond = 360 / 60; // 60 seconds in a minute
+
+    const initialRotationDegree =
+      (360 - initialSeconds * degreesPerSecond) % 360;
+    setRotationDegree(initialRotationDegree);
+
     const intervalId = setInterval(() => {
       updateRotationDegree();
     }, 1000);
 
     return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    console.log("CountdownZero prop has changed:", countdownZero);
+
+    if (countdownZero) {
+      console.log("Countdown zero, shouldRunAnimation set to false");
+      setShouldRunAnimation(false); // Stop the animation when countdown seconds are zero
+      console.log("show", shouldRunAnimation);
+    } else {
+      console.log("Countdown not zero, shouldRunAnimation set to true");
+      setShouldRunAnimation(true); // Resume the animation when countdown seconds are not zero
+    }
   }, [countdownZero]);
 
   function updateRotationDegree() {
@@ -19,15 +43,11 @@ function Seconds({ countdownZero }) {
     const seconds = now.getSeconds();
     const degreesPerSecond = 360 / 60; // 60 seconds in a minute
 
-    if (countdownZero) {
-      setRotationDegree(0);
-    } else {
-      const newRotationDegree = (360-(seconds * degreesPerSecond)) % 360;
+    if (!countdownZero) {
+      const newRotationDegree = (360 - seconds * degreesPerSecond) % 360;
       setRotationDegree(newRotationDegree);
     }
   }
-
-
   return (
     <>
       <div className="countdown-row w-full flex hidden md:block">
@@ -48,7 +68,9 @@ function Seconds({ countdownZero }) {
                 alt="Clocks seconds"
                 style={{
                   transform: `rotate(${rotationDegree}deg)`,
-                  transition: "transform 0.3s ease", // Add CSS transition
+                  transition: shouldRunAnimation
+                    ? "transform 0.4s ease" // Adjust transition time to match the rotation speed
+                    : "none", // Add CSS transition
                 }}
               />
             </div>
@@ -56,7 +78,9 @@ function Seconds({ countdownZero }) {
               className="down_opacity_circle "
               style={{
                 transform: `rotate(${rotationDegree}deg)`,
-                transition: "transform 0.3s ease", // Add CSS transition
+                transition: shouldRunAnimation
+                  ? "transform 0.4s ease" // Adjust transition time to match the rotation speed
+                  : "none", // Add CSS transition
               }}
             >
               <img
@@ -88,7 +112,9 @@ function Seconds({ countdownZero }) {
                 alt="Clocks seconds"
                 style={{
                   transform: `rotate(${rotationDegree}deg)`,
-                  transition: "transform 0.3s ease", // Add CSS transition
+                  transition: shouldRunAnimation
+                    ? "transform 1s ease" // Adjust transition time to match the rotation speed
+                    : "none", // Add CSS transition
                 }}
               />
             </div>
@@ -97,7 +123,9 @@ function Seconds({ countdownZero }) {
               className="down_opacity_circle  "
               style={{
                 transform: `rotate(${rotationDegree}deg)`,
-                transition: "transform 0.3s ease", // Add CSS transition
+                transition: shouldRunAnimation
+                  ? "transform 1s ease" // Adjust transition time to match the rotation speed
+                  : "none", // Add CSS transition
               }}
             >
               <img
